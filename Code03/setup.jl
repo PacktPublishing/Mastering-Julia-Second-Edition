c6cf9dedc318989fa60f9adf78e6677fe0566a3d
Malcolm Sherrington <malcolm.sherrington@gmail.com>
2023-10-30 11:01:21 +0000

    Update TOML files

--- Code03/setup.jl
+++ Code03/setup.jl
@@ -1,10 +1,10 @@
 #!/usr/local/bin/julia
 #
 isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
-
 const PKGS = ["BenchmarkTools","Match","StaticArrays","LinearAlgebra"]
 
 using Pkg
+Pkg.activate(".")
 println("Installing required packages ...")
 
 for p in PKGS
