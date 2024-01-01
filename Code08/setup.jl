668a7d24280e1b6aefe45a64cbd8d60bbfde09e3
Malcolm Sherrington <malcolm.sherrington@gmail.com>
2024-01-01 16:46:42 +0000

    First upload

---
+++ Code08/setup.jl
@@ -0,0 +1,16 @@
+#! /usr/bin/env julia
+#
+# Note: Assume OSX and an alias setup to julia in /usr/local/bin
+#
+isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
+
+const PKGS = ["UnicodePlots","PyPlot","Luxor","Winston"]
+
+using Pkg
+Pkg.activate(".")
+
+println("Installing required packages ...")
+for p in PKGS
+  isinstalled(p) || Pkg.add(p)
+end
+println("Done!")
