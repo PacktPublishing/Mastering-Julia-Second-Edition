72432f9d1afadd7c724a8ed6a057790da7fd7da1
Malcolm Sherrington <malcolm.sherrington@gmail.com>
2024-01-10 15:44:08 +0000

    Tweak README.md and setuo.jl files

--- Code11/setup.jl
+++ Code11/setup.jl
@@ -1,5 +1,16 @@
-#!/usr/local/bin/julia
+#!/usr/bin/env julia
 #
+# Note: Assume MacOS/Linux
+#
+
+# Delete any existing TOML files
+
+println("\nDeleting any TOML files ...")
+rm("./Project.toml",force=true)
+rm("./Manifest.toml",force=true)
+
+# Create new TOML files
+
 isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
 
 const PKGS = ["ArgParse","Getopt","BenchmarkTools","Example","Plots","Statistics","Traceur","Debugger","Revise","StatProfilerHTML"]
@@ -10,4 +21,6 @@ println("Installing required packages ...")
 for p in PKGS
   isinstalled(p) || Pkg.add(p)
 end
-println("Done!")
+
+println("\nListing installed packages ...")
+Pkg.status()
