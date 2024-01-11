1319c955ab8c26cfe6cec2f032423080112772a0
Malcolm Sherrington <malcolm.sherrington@gmail.com>
2024-01-11 09:01:22 +0000

    Create setup.jl

---
+++ Code08/Gadfly/setup.jl
@@ -0,0 +1,26 @@
+#!/usr/bin/env julia
+#
+# Note: Assume MacOS/Linux
+#
+
+# Delete any existing TOML files
+
+println("\nDeleting any TOML files ...")
+rm("./Project.toml",force=true)
+rm("./Manifest.toml",force=true)
+
+# Create new TOML files
+
+isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
+const PKGS = ["Gadfly","Compose","RDatasets","DataFrames","Fontconfig","IJulia"]
+
+using Pkg
+Pkg.activate(".")
+
+println("Installing required packages ...")
+for p in PKGS
+  isinstalled(p) || Pkg.add(p)
+end
+
+println("\nListing installed packages ...")
+Pkg.status()
