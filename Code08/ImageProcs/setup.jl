0468e53c63a7c7631d05425da547bcb6246fbd5c
Malcolm Sherrington <malcolm.sherrington@gmail.com>
2024-01-11 09:21:05 +0000

    Update setup.jl

--- Code08/ImageProcs/setup.jl
+++ Code08/ImageProcs/setup.jl
@@ -1,15 +1,26 @@
 #!/usr/bin/env julia
 #
-# Note: Script to work with MacOS or Linux
+# Note: Assume MacOS/Linux
 #
-isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
 
+# Delete any existing TOML files
+
+println("\nDeleting any TOML files ...")
+rm("./Project.toml",force=true)
+rm("./Manifest.toml",force=true)
+
+# Create new TOML files
+
+isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
 const PKGS = ["Images", "ImageView", "Colors", "ImageFiltering", "MosaicViews",
               "ImageEdgeDetection", "ImageTransformations"]
-
 using Pkg
+Pkg.activate(".")
+
 println("Installing required packages ...")
 for p in PKGS
   isinstalled(p) || Pkg.add(p)
 end
-println("Done!")
+
+println("\nListing installed packages ...")
+Pkg.status()
